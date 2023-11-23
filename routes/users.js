8681b957ab0a4284eb4bef9e926ada22af2c2833
c2toto00 <c2toto00@students.oamk.fi>
2023-11-23 11:53:23 +0200

    added user data CRUD operations

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const { getUserByUsername, getUsers } = require("../postgre/user");
+
 /* GET users listing. */
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     res.send("respond with a resource");
+    const users = await getUsers();
+    console.log(users);
 });
 
 module.exports = router;
