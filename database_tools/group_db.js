c6f8dec29d55ce012f400f2c1168053b3ac66129
asrii00 <t2rias00@students.oamk.fi>
2023-11-24 14:22:42 +0200

    workin on it

--- database_tools/group_db.js
+++ database_tools/group_db.js
@@ -1,4 +1,4 @@
-//require connection
+const pgPool = require("../connection");
 
 const sql = {
     GET_GROUP: 'SELECT name, description, avatar_url FROM groups WHERE group_id=$1',
@@ -7,32 +7,49 @@ const sql = {
     ADD_GROUP_POST: '',
     GET_GROUP_MEMBERS: 'SELECT user_id FROM user_groups WHERE group_id=$1',
     ADD_GROUP_MEMBER: 'INSERT INTO user_groups (user_id, group_id) VALUES ($1, $2)', 
-    DELETE_GROUP_MEMBER: '' 
+    DELETE_GROUP_MEMBER: 'DELETE FROM user_groups WHERE user_id=$1' 
 }
 
 
 async function getGroup(groupId){
 
-    if (groupId){
+        let result = await pgPool.query(sql.GET_GROUP, [groupId]);
 
-        let result = ''; //query
-    }
-    else {
-
-        return null;
-    }
-}
-async function addGroup(groupName, groupDescription){
-
-    if (groupId){
-
-        let result = ''; //query
-    }
-    else {
-
-        return null;
-    }
+        if(result.rows.length>0){
+            return result.rows[0];
+         }else{
+            return null;
+         }
 }
 
+async function getGroupMembers(groupId){
 
-module.exports = {getGroup, addGroup};
+        let result = await pgPool.query(sql.GET_GROUP_MEMBERS, [groupId]);
+
+        if(result.rows.length>0){
+            const groupMembersArray = result.map(item => item.user_id);
+            return groupMembersArray; //idk if this is the best move
+         }else{
+            return null;
+         }
+}
+
+async function addGroup(groupName, groupDescription, groupAvatar){
+
+    const addGroupResult = await pgPool.query(sql.ADD_GROUP, [groupName, groupDescription, groupAvatar]);
+    return addGroupResult.rows[0];
+    //error handling?
+    
+}
+
+async function addGroupPost(groupId, postContent){
+
+    const addPostResult = await pgPool.query(ADD_GROUP_POST, [groupId, postContent]);
+    return addPostResult.rows[0];
+    //do any errors need to be checked for here ._. 
+}
+
+
+
+
+module.exports = {getGroup, addGroup, addGroupPost, getGroupMembers};
--- routes/groups.js
+++ routes/groups.js
@@ -1,22 +1,58 @@
 const express = require("express");
 const router = express.Router();
+const pgPool = require("../connection");
+const {getGroup, addGroup, addGroupPost } = require('../database_tools/group_db');
 
 // GET by group id (auth?)
 // => group id, name, description, avatar url
 
-router.get("/:groupId", (req, res) => {
+router.get("/:groupId", async (req, res) => {
     
     try {
         const groupId = req.params.groupId;
-         //database fetch 
-
-
-
-        res.status(200).json({});
+        const result = await getGroup(groupId);
+        res.status(201).json({result});
     } catch (error) {
         console.log(error);
         res.status(500).json({error: 'Internal Server Error'}); //tms.
     }
 });
+
+//gname, gdesc, gavatar 
+router.post('/add', async (req,res) => {
+    const gname = req.body.gname;
+    const gdesc = req.body.gdesc;
+    const gavatar = req.body.gavatar;
+
+    const client = await pgPool.connect(); 
+
+    try{
+        const result = await addGroup(gname, gdesc, gavatar);
+        //const newGroup = result.rows[0];
+        res.status(201).json({ message: "Group created successfully", result });
+
+    } catch (error){
+        console.error("Error while interacting with the database:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+        client.release(); 
+    }
+});
+
+
+router.post('/post', async (req, res)=>{
+    try {
+        const result = await addGroupPost(req.body.groupid, req.body.postcontent);
+        if(result){ ///??
+            res.status(200).send('Post added to group ' + groupid);
+        }else{
+            res.status(404).send('Group ' + req.body.groupid + ' not found!');    
+        }
+    } catch (error) {
+        res.status(401).json({error: error.message});
+    }
+});
+
+
 
 module.exports = router;
