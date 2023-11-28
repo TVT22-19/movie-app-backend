1d3faaf9224f6a998327e46e95617c00c768b78d
c2toto00 <c2toto00@students.oamk.fi>
2023-11-28 12:35:32 +0200

    added error handling

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-const { getUserByUsername, getUsers } = require("../postgre/user");
+const { getUsers, getUserById } = require("../database_tools/user");
 
 /* GET users listing. */
 router.get("/", async (req, res) => {
-    res.send("respond with a resource");
-    const users = await getUsers();
-    console.log(users);
+    try{
+        const users = await getUsers();
+        res.send(users);
+    }catch(error){
+        console.error("Error with database connection");
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+/* Get user by ID */
+router.get("/:id", async (req, res) => {
+    if(!req.params.id){
+        return res.status(400).json({ error: "User ID is required" });
+    }
+
+    try{
+        const user = await getUserById(req.params.id);
+        res.send(user);
+    }catch(error){
+        console.error("Error with database connection");
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 module.exports = router;
