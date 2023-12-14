c4c1000fb1fd4f6ff45a5f1defb73490cee53180
c2toto00 <c2toto00@students.oamk.fi>
2023-12-14 16:05:58 +0200

    login/register moved under auth

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,13 @@ const {getUsers} = require("../database_tools/user");
 
 const jwtSecret = process.env.JWT_SECRET || "default-secret-key";
 
-router.post("/", async (req, res) => {
+// Helper function to hash passwords
+const hashPassword = async (password) => {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds);
+};
+
+router.post("/login", async (req, res) => {
     const { username, password } = req.body;
 
     if (!username || !password) {
@@ -37,6 +43,36 @@ router.post("/", async (req, res) => {
 
         console.log(`User ${username} successfully logged in`);
         res.status(200).json({ message: "Login successful", user: user, token: token });
+    } catch (error) {
+        console.error("Error while interacting with the database:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+router.post("/registration", async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ error: "Username and password are required" });
+    }
+
+    try {
+        // Checking if user exists
+        const userExistsQuery = "SELECT * FROM users WHERE username = $1";
+        const userExistsResult = await pgPool.query(userExistsQuery, [username]);
+
+        if (userExistsResult.rows.length > 0) {
+            return res.status(400).json({ error: "Username already exists" });
+        }
+
+        const hashedPassword = await hashPassword(password);
+
+        // Added new user
+        const addUserQuery = "INSERT INTO users(username, password) VALUES ($1, $2) RETURNING *";
+        const addUserResult = await pgPool.query(addUserQuery, [username, hashedPassword]);
+        const newUser = addUserResult.rows[0];
+
+        res.status(201).json({ message: "User created successfully", user: newUser });
     } catch (error) {
         console.error("Error while interacting with the database:", error);
         res.status(500).json({ error: "Internal Server Error" });
